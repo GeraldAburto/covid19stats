b35facb479b703e75bd96c89afb466fae00d3332
Gerald Aburto <gerald.aburto@efika.solutions>
2020-07-30 22:24:52 -0600

    Added routes and basic pages

---
+++ src/Routes.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import AboutPage from './pages/AboutPage/AboutPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+
+const Routes = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/about" component={AboutPage} />
+      <Route path="*" component={NotFoundPage} />
+    </Switch>
+  </Router>
+);
+
+export default Routes;
---
+++ src/layouts/DefaultLayout/DefaultLayout.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const DefaultLayout = ({ children }) => <>{children}</>;
+
+DefaultLayout.prototype = {
+  children: PropTypes.node.isRequired,
+};
+
+export default DefaultLayout;
