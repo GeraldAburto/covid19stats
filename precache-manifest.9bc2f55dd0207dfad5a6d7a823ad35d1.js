9462c6e9784e84375ff18013f81d00ca525614db
Gerald Aburto <gerald.aburto@efika.solutions>
2020-08-02 16:51:48 -0600

    Updates

---
+++ precache-manifest.9bc2f55dd0207dfad5a6d7a823ad35d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb57b447cd37a8fcbcd2a55518e9b2e4",
+    "url": "/covid19stats/index.html"
+  },
+  {
+    "revision": "27bc294b8041cfbcd7a1",
+    "url": "/covid19stats/static/css/main.d811a0a4.chunk.css"
+  },
+  {
+    "revision": "b8d9fa273565b030576d",
+    "url": "/covid19stats/static/js/2.d62310f7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid19stats/static/js/2.d62310f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27bc294b8041cfbcd7a1",
+    "url": "/covid19stats/static/js/main.70783ab7.chunk.js"
+  },
+  {
+    "revision": "d6794d6704fe094387d1",
+    "url": "/covid19stats/static/js/runtime-main.bcca0e7d.js"
+  },
+  {
+    "revision": "bf733151b8ca3454a47c2842944bc683",
+    "url": "/covid19stats/static/media/virus.bf733151.svg"
+  }
+]);
