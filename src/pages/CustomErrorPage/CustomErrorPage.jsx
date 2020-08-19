463f2ce18a0e3133fb13fefdcefb1c1bab3a590e
Gerald Aburto Urroz <aburtogerald@gmail.com>
2020-08-19 17:51:29 -0600

    Added back to home button on not found and error page

--- src/pages/CustomErrorPage/CustomErrorPage.jsx
+++ src/pages/CustomErrorPage/CustomErrorPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { withRouter } from 'react-router-dom';
+import { Row, Col, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeartBroken } from '@fortawesome/free-solid-svg-icons';
 
-const CustomErrorPage = () => (
+const CustomErrorPage = withRouter(({ history }) => (
   <Row>
     <Col sm={12} className="text-center">
       <p style={{ fontSize: '40px' }}>
@@ -12,7 +13,10 @@ const CustomErrorPage = () => (
         Something went wrong.
       </p>
     </Col>
+    <Col sm={12} className="text-center">
+      <Button onClick={() => history.push('/')}>Back to home page</Button>
+    </Col>
   </Row>
-);
+));
 
 export default CustomErrorPage;
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { withRouter } from 'react-router-dom';
+import { Row, Col, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSadTear } from '@fortawesome/free-solid-svg-icons';
 
-const NotFoundPage = () => (
+const NotFoundPage = withRouter(({ history }) => (
   <Row>
     <Col sm={12} className="text-center">
       <p style={{ fontSize: '40px' }}>
@@ -12,7 +13,10 @@ const NotFoundPage = () => (
         Not Found.
       </p>
     </Col>
+    <Col sm={12} className="text-center">
+      <Button onClick={() => history.push('/')}>Back to home page</Button>
+    </Col>
   </Row>
-);
+));
 
 export default NotFoundPage;
